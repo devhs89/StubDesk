7c3aef28d2accbea1583867a1727a6c6bb309be8
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-06-15 19:37:20 -0400

    REFACTOR: remove navlinks

--- client/src/components/common/AppNavbar.js
+++ client/src/components/common/AppNavbar.js
@@ -1,25 +1,14 @@
-import NavLink from "react-bootstrap/NavLink";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
 
 export const AppNavbar = () => {
   return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow sticky-top">
     <div className="container-fluid">
-      <NavbarBrand href="/">Navbar</NavbarBrand>
+      <NavbarBrand href="/">StubDesk</NavbarBrand>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
               aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <NavLink aria-current="page" href="/home">Home</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink>Directory</NavLink>
-          </li>
-        </ul>
-      </div>
     </div>
   </nav>);
 };
