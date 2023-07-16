e91c862180a0a50df9716b3b85dee6fcd44c9368
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-07-16 19:38:29 -0400

    FEATURE: 2023 July 16: implement 404 page

--- ui/src/components/AppRouter.jsx
+++ ui/src/components/AppRouter.jsx
@@ -3,12 +3,22 @@ import {Navigate, Route, Routes} from 'react-router-dom';
 import {Home} from "./Home.jsx";
 import {EmployeeDirectory} from "./EmployeeDirectory.jsx";
 
+class Page404 extends React.Component {
+  render() {
+    return <>
+      <div className="w-100 display-4 mb-3 text-danger">Error 404</div>
+      <div className="fw-semibold">Page not Found</div>
+    </>;
+  }
+}
+
 export class AppRouter extends React.Component {
   render() {
     return (<Routes>
-      <Route path={'/home'} Component={Home}></Route>
-      <Route path={'/employees'} Component={EmployeeDirectory}></Route>
+      <Route path={'home'} Component={Home}></Route>
+      <Route path={'employees'} Component={EmployeeDirectory}></Route>
       <Route path="/" element={<Navigate replace to={'/home'} />}></Route>
+      <Route path="*" element={<Page404 />}></Route>
     </Routes>);
   }
 }
