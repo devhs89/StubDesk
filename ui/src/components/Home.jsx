b8aa1a5a73f9576b6777b19280d951d685c44dcc
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-07-17 23:30:54 -0400

    FIX: 2023 July 17: add employee directory link button to home page

--- ui/src/components/Home.jsx
+++ ui/src/components/Home.jsx
@@ -1,10 +1,17 @@
 import {Component} from "react";
+import {Col, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 export class Home extends Component {
   render() {
     return (<>
       <h1>Welcome</h1>
       <p>Click the button below to navigate to employee directory.</p>
+      <Row>
+        <Col className="mb-3">
+          <Link className="btn btn-primary" to="/employee/directory">Employee Directory</Link>
+        </Col>
+      </Row>
     </>);
   }
 }
